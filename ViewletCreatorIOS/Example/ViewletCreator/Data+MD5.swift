9fda01ecf87d36c20750c71a9aa231d54b1a3b27
Johan Bos <johan.bos@kpn.com>
2019-03-27 13:33:28 +0100

    Performed swift 5 conversion and removed warning

--- ViewletCreatorIOS/Example/ViewletCreator/Data+MD5.swift
+++ ViewletCreatorIOS/Example/ViewletCreator/Data+MD5.swift
@@ -10,16 +10,13 @@ import Foundation
 extension Data {
 
     func md5() -> String {
-        let context = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
-        var digest = Array<UInt8>(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-        CC_MD5_Init(context)
-        let _ = self.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-            CC_MD5_Update(context, body, CC_LONG(self.count))
+        let hash = self.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(self.count), &hash)
+            return hash
         }
-        CC_MD5_Final(&digest, context)
-        context.deallocate()
         var hexString = ""
-        for byte in digest {
+        for byte in hash {
             hexString += String(format:"%02x", byte)
         }
         return hexString
